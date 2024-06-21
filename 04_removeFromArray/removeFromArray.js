ae31baddc997224014c8350e8c56204456ebb1c6
alex-wichman <alex.d.wichman@gmail.comgit config --global user.name alex-wichmangit config --global user.name alex-wichmanexitclear>
2024-06-20 21:19:55 -0500

    passed first test case

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, num) {
+    for (let i = 0; i < array.length; i++)
+        {
+            if (array[i] == num)
+                {
+                    array.splice(i,1);
+                }
+        }
+        return array;
 };
 
 // Do not edit below this line
