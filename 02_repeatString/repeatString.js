61ffdabdfb1b69593a3ff79ae0890f4d1f7e238d
alex-wichman <alex.d.wichman@gmail.comgit config --global user.name alex-wichmangit config --global user.name alex-wichmanexitclear>
2024-06-20 21:06:56 -0500

    all test cases passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,14 @@
-const repeatString = function() {
+const repeatString = function(word, num) {
+    let answer = "";
+    if (num < 0)
+        {
+            return "ERROR";
+        }
+    for (let i = 0; i < num; i++)
+        {
+            answer += word;
+        }
+    return answer;
 
 };
 
