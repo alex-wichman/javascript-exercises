39fbdd98e9022afc7800f8a5e57fab502d129f25
alex-wichman <alex.d.wichman@gmail.comgit config --global user.name alex-wichmangit config --global user.name alex-wichmanexitclear>
2024-06-21 21:18:34 -0500

    all test cases passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let sum = 0;
+    
+    if (start < 0 || end < 0 || !Number.isInteger(start) || !Number.isInteger(end))
+        {
+            return "ERROR";
+        }
 
+    let max = Math.max(start,end);
+    let min = Math.min(start,end);
+    for (min; min <= max; min++)
+        {
+            sum += min;
+        }
+        return sum;
 };
 
 // Do not edit below this line
