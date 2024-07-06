b05dc92cc1ada18b1e03f3e33245013ab862a05e
alex-wichman <alex.d.wichman@gmail.comgit config --global user.name alex-wichmangit config --global user.name alex-wichmanexitclear>
2024-07-05 22:56:16 -0500

    finished exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,22 @@
-const leapYears = function() {
+const leapYears = function(num) {
+
+    if (num % 4 == 0 && num % 100 == 0 && num % 400 == 0)
+        {
+            return true;
+        }
+    if (num % 4 == 0 && num % 100 == 0 && num % 400 != 0)
+        {
+            return false;
+        }
+    if (num % 4 == 0 && num % 100 != 0 && num % 400 != 0)
+        {
+            return true;
+        }
+    if (num % 4 != 0)
+        {
+            return false;
+        }
+        
 
 };
 
