af4b078e20c001a93938aac98d17e677a8fceb64
alex-wichman <alex.d.wichman@gmail.comgit config --global user.name alex-wichmangit config --global user.name alex-wichmanexitclear>
2024-07-05 22:43:25 -0500

    5th exercise done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,13 +1,16 @@
-const removeFromArray = function(array, num) {
-    for (let i = 0; i < array.length; i++)
-        {
-            if (array[i] == num)
-                {
-                    array.splice(i,1);
-                }
+const removeFromArray = function(array, ...args) {
+    
+    let newArray = [];
+
+    array.forEach((item) => {
+        // push every element into the new array
+        // UNLESS it is included in the function arguments
+        // so we create a new array with every item, except those that should be removed
+        if (!args.includes(item)) {
+          newArray.push(item);
         }
-        return array;
-};
+    });
+    return  newArray;
 
 // Do not edit below this line
 module.exports = removeFromArray;
